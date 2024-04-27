a5403e1657712f35c68cc769271b5775f5bde83f
Justin Porter <jp88@rice.edu>
2024-04-27 11:55:48 -0500

    Added test for AFT with multiple unilateral springs added at once

--- tests/nlforces/test_unilateral_spring.py
+++ tests/nlforces/test_unilateral_spring.py
@@ -57,8 +57,13 @@ class TestUniSpring(unittest.TestCase):
             
 
         self.uni_springs = uni_springs
-        
-        
+
+
+        Q_all = np.array([[1.0, -1.0, 2.0, -2.0, 1.7]]).T
+
+        self.uni_spring_all = UnilateralSpring(Q_all, Q_all.T, kuni,
+                                               Npreload=Npre, delta=delta)
+
         # Code for an example plotting what each setting looks like
         """
         import matplotlib as mpl
@@ -162,6 +167,73 @@ class TestUniSpring(unittest.TestCase):
                 
                 self.assertFalse(grad_failed, 'Incorrect frequency gradient.')
                 
+    def test_multiple_aft(self):
+        """
+        Test the multiple springs AFT
+
+        Returns
+        -------
+        None.
+
+        """
+
+        uni_springs = self.uni_springs
+
+        Nd = 1
+
+        h = np.array([0, 1, 2, 3, 4, 5, 6, 7])
+        w = 1.0
+
+        Nhc = hutils.Nhc(h)
+
+        U = np.zeros((Nd*Nhc, 1))
+
+        rng = np.random.default_rng(seed=1023)
+
+        # Test several different values of U on different length scales for
+        # each spring type
+        U = rng.rand(Nd*Nhc, 10)
+
+        U = U*np.array([[0.1, 0.5, 1.0, 2.0, 3.0, 10.0, 20.0, 50.0, 100.0, 0.01]])
+
+        Q_together = self.uni_spring_all.Q
+
+        for j in range(U.shape[1]):
+
+            U_curr = U[:, j]
+            Fnl_tot = np.zeros_like(U_curr)
+
+            for i in range(len(uni_springs)):
+
+                # Externally applying the Q map from the combined one to each
+                # invididual case here
+                Fnl_tot += Q_together[i]*uni_springs[i].aft(
+                                                        U_curr*Q_together[i],
+                                                        w, h)[0]
+
+            Fnl_together = self.uni_spring_all.aft(U_curr, w, h)[0]
+
+            self.assertLess(np.linalg.norm(Fnl_tot-Fnl_together), 1e-12,
+                            'Unilateral springs togther does not give same '
+                            + 'results as separate ones.')
+
+            fun = lambda U: self.uni_spring_all.aft(U, w, h)[0:2]
+            grad_failed = vutils.check_grad(fun, U_curr, verbose=False,
+                                            atol=self.atol_grad,
+                                            rtol=self.rtol_grad)
+
+            self.assertFalse(grad_failed,
+                             'Combined AFT, Incorrect displacement gradient.')
+
+            # Numerically Verify Frequency Gradient
+            fun = lambda w: self.uni_spring_all.aft(U_curr, w[0], h)[0::2]
+            grad_failed = vutils.check_grad(fun, np.array([w]),
+                                            verbose=False,
+                                            rtol=self.rtol_grad)
+
+            self.assertFalse(grad_failed,
+                             'Combined AFT, Incorrect frequency gradient.')
+
 
 
 if __name__ == '__main__':
